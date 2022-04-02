05263394e87e21f2a33eb8d2f29d3d89a57b27bc
kovachevx <hristo.kovachev95@gmail.com>
2022-04-02 19:22:52 +0300

    Checking if password is correct. Renamed description file.

---
+++ Programming-Basics-JS/02. Conditional-Statements-Lab/04. passwordGuess.js
@@ -0,0 +1,12 @@
+function password(input) {
+
+    let password = input[0];
+
+    if (password === "s3cr3t!P@ssw0rd") {
+        console.log("Welcome");
+    } else {
+        console.log("Wrong password!");
+    }
+
+}
+password(["s3cr3t!P@ssw0rd"]);
