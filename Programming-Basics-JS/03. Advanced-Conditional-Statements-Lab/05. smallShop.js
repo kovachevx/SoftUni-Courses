531509bb0c23a90885b75ec98088fb212c36b127
kovachevx <hristo.kovachev95@gmail.com>
2022-04-07 21:07:31 +0300

    Estimating final price based on city, product & quantity.

---
+++ Programming-Basics-JS/03. Advanced-Conditional-Statements-Lab/05. smallShop.js
@@ -0,0 +1,36 @@
+function smallShop(input) {
+
+    let product = input[0];
+    let city = input[1];
+    let quantity = Number(input[2]);
+    let finalPrice = 0;
+
+    if (city === 'Sofia') {
+        switch (product) {
+            case 'coffee': finalPrice = quantity * 0.50; break;
+            case 'water': finalPrice = quantity * 0.80; break;
+            case 'beer': finalPrice = quantity * 1.20; break;
+            case 'sweets': finalPrice = quantity * 1.45; break;
+            case 'peanuts': finalPrice = quantity * 1.60; break;
+        }
+    } else if (city === 'Plovdiv') {
+        switch (product) {
+            case 'coffee': finalPrice = quantity * 0.40; break;
+            case 'water': finalPrice = quantity * 0.70; break;
+            case 'beer': finalPrice = quantity * 1.15; break;
+            case 'sweets': finalPrice = quantity * 1.30; break;
+            case 'peanuts': finalPrice = quantity * 1.50; break;
+        }
+    } else if (city === 'Varna') {
+        switch (product) {
+            case 'coffee': finalPrice = quantity * 0.45; break;
+            case 'water': finalPrice = quantity * 0.70; break;
+            case 'beer': finalPrice = quantity * 1.10; break;
+            case 'sweets': finalPrice = quantity * 1.35; break;
+            case 'peanuts': finalPrice = quantity * 1.55; break;
+
+        }
+    }
+    console.log(finalPrice)
+}
+smallShop(['peanuts', 'Plovdiv', 1])
