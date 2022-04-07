33f2d0a57cb403ea40d2a244142ad142ab384b96
kovachevx <hristo.kovachev95@gmail.com>
2022-04-07 21:19:04 +0300

    Determining if a store is open on a given day & hour.

---
+++ Programming-Basics-JS/03. Advanced-Conditional-Statements-Lab/07. workingHours.js
@@ -0,0 +1,30 @@
+function workingHours(input) {
+
+    let hour = Number(input[0]);
+    let day = input[1];
+
+    if (hour >= 10 && hour < 18) {
+        switch (day) {
+            case 'Monday':
+            case 'Tuesday':
+            case 'Wednesday':
+            case 'Thursday':
+            case 'Friday':
+            case 'Saturday': console.log('open'); break;
+            case 'Sunday': console.log('closed'); break;
+            default: console.log('closed'); break;
+        }
+    }
+    else if (hour < 10 || hour >= 18) {
+        switch (day) {
+            case 'Monday':
+            case 'Tuesday':
+            case 'Wednesday':
+            case 'Thursday':
+            case 'Friday':
+            case 'Saturday':
+            case 'Sunday': console.log('closed'); break;
+        }
+    }
+}
+workingHours([19, 'Tuesday']);
