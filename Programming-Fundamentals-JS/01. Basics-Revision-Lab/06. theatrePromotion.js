ef4f3bf8ef806aea80646ef48db697660dd0dc64
kovachevx <hristo.kovachev95@gmail.com>
2022-04-30 15:47:47 +0300

    Calculating discount based on day & customer age.

---
+++ Programming-Fundamentals-JS/01. Basics-Revision-Lab/06. theatrePromotion.js
@@ -0,0 +1,34 @@
+function theatrePromotion(day, age) {
+
+    let price;
+    let isValid = true;
+
+    if (age >= 0 && age <= 18){
+        switch(day){
+            case 'Weekday': price = 12; break;
+            case 'Weekend': price = 15; break;
+            case 'Holiday': price = 5; break;
+        }
+    } else if (age > 18 && age <= 64) {
+        switch(day){
+            case 'Weekday': price = 18; break;
+            case 'Weekend': price = 20; break;
+            case 'Holiday': price = 12; break;
+        }
+    } else if (age > 64 && age <= 122){
+        switch(day){
+            case 'Weekday': price = 12; break;
+            case 'Weekend': price = 15; break;
+            case 'Holiday': price = 10; break;
+        }
+    } else {
+        console.log('Error!');
+        isValid = false;
+    }
+
+    if (isValid) {
+    console.log(`${price}$`);
+    }
+}
+
+theatrePromotion("Holiday", 5)
