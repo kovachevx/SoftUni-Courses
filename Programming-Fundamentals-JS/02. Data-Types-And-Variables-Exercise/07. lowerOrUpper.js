18ca2f295e6efb8517997e66f0b5a74d40485c92
kovachevx <hristo.kovachev95@gmail.com>
2022-05-09 11:30:43 +0300

    Determining whether an input characters is written in lower or upper case.

---
+++ Programming-Fundamentals-JS/02. Data-Types-And-Variables-Exercise/07. lowerOrUpper.js
@@ -0,0 +1,9 @@
+function lowerOrUpper(char) {
+    let upChar = char.toUpperCase();
+    console.log(upChar === char ? 'upper-case' : 'lower-case');
+}
+lowerOrUpper('L');
+lowerOrUpper('f');
+
+// let lowChar = char.toLowerCase();
+// console.log(lowChar == char ? 'lower-case' : 'upper-case');
