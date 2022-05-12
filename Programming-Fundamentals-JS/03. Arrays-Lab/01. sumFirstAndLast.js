2b02f1026346d1c9990e2dc051aba0d5ced4402c
kovachevx <hristo.kovachev95@gmail.com>
2022-05-12 21:39:43 +0300

    Summing up the first & last elements of an array.

---
+++ Programming-Fundamentals-JS/03. Arrays-Lab/01. sumFirstAndLast.js
@@ -0,0 +1,10 @@
+function sumFirstAndLast(arr) {
+    let first = arr[0];
+    let index = arr.length - 1;
+    let last = arr[index];
+
+    console.log(first + last);
+}
+sumFirstAndLast([20, 30, 40]);
+sumFirstAndLast([10, 17, 22, 33]);
+sumFirstAndLast([11, 58, 69]);
