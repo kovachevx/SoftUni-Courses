0182cade4d8025cbc9b9ab4bd53e5cb3e6b7fc19
kovachevx <hristo.kovachev95@gmail.com>
2022-05-17 21:39:23 +0300

    Extracting numbers from an array that form a non-decreasing subset.

---
+++ Programming-Fundamentals-JS/03. Arrays-MoreExercises/04. nonDecreasingSubset.js
@@ -0,0 +1,15 @@
+function nonDecreasingSubset(arr) {
+
+    let max = arr[0];
+    let res = arr.filter(el => {
+        if (el >= max) {
+            max = el;
+        }
+        return el >= max;
+    });
+
+    console.log(res.join(' '));
+}
+nonDecreasingSubset([1, 3, 8, 4, 10, 12, 3, 2, 24]);
+nonDecreasingSubset([1, 2, 3, 4]);
+nonDecreasingSubset([20, 3, 2, 15, 6, 1]);
