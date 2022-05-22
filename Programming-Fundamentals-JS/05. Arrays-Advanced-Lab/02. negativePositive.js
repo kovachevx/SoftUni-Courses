55605d0a5780609f1d73fdd035c189cc372396fe
kovachevx <hristo.kovachev95@gmail.com>
2022-05-22 15:27:11 +0300

    Placing numbers at the begging/end of an array based on whether they are positive.

---
+++ Programming-Fundamentals-JS/05. Arrays-Advanced-Lab/02. negativePositive.js
@@ -0,0 +1,14 @@
+function negativePositive(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            result.unshift(arr[i]);
+        } else {
+            result.push(arr[i])
+        }
+    }
+    console.log(result.join('\n'));
+}
+negativePositive(['7', '-2', '8', '9']);
+negativePositive(['3', '-2', '0', '-1']);
+
