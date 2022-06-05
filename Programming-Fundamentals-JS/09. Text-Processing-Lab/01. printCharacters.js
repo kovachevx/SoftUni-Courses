ffb0e9d7d6aabb44bb1c3a841704b428a931faf7
kovachevx <hristo.kovachev95@gmail.com>
2022-06-05 11:48:09 +0300

    Receiving a string & printing all its characters on separate lines.

---
+++ Programming-Fundamentals-JS/09. Text-Processing-Lab/01. printCharacters.js
@@ -0,0 +1,10 @@
+function printCharacters(word) {
+    for (let letter of word) {
+        console.log(letter);
+    }
+}
+printCharacters('AWord');
+printCharacters('Sentence');
+
+// let printChars = (word) => word.forEach((x) => console.log(x));
+// printChars('Aword');
