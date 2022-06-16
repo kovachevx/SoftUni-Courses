3f1ac502ae04054f5459133e53c5df4b627b3256
kovachevx <hristo.kovachev95@gmail.com>
2022-06-16 15:48:54 +0300

    Exercising syntax, functions & statements.

---
+++ Programming-JS-Advanced/01. Syntax-Functions-and-Statements-Lab/03. largestNumber.js
@@ -0,0 +1,7 @@
+(num1, num2, num3) => `The largest number is ${Math.max(num1, num2, num3)}.`;
+
+// function largestNumber(num1, num2, num3) {
+//     console.log(`The largest number is ${Math.max(num1, num2, num3)}.`);
+// }
+// largestNumber(5, -3, 16);
+// largestNumber(-3, -5, -22.5);
