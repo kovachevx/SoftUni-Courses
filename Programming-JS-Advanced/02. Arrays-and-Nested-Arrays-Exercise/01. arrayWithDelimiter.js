fb1a01e3547bfb1b2e5ef8517619cd8d42a1c783
kovachevx <hristo.kovachev95@gmail.com>
2022-06-22 13:50:08 +0300

    Practising modifying arrays & nested arrays.

---
+++ Programming-JS-Advanced/02. Arrays-and-Nested-Arrays-Exercise/01. arrayWithDelimiter.js
@@ -0,0 +1,16 @@
+function arrayWithDelimiter(array, delimiter) {
+    console.log(array.join(delimiter));
+}
+arrayWithDelimiter(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-');
+arrayWithDelimiter(['How about no?',
+    'I',
+    'will',
+    'not',
+    'do',
+    'it!'],
+    '_');
