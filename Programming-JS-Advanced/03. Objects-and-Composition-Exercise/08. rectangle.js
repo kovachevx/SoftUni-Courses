64cea2ab45131d669bfa78d6edf96477e6a643c8
kovachevx <hristo.kovachev95@gmail.com>
2022-06-26 22:26:05 +0300

    Practising object modification & composition.

---
+++ Programming-JS-Advanced/03. Objects-and-Composition-Exercise/08. rectangle.js
@@ -0,0 +1,21 @@
+function rectangle(width, height, color) {
+
+    color = color[0].toUpperCase() + color.substring(1);
+
+    let output = {
+        width,
+        height,
+        color,
+        calcArea() {
+            return this.width * this.height;
+        }
+    };
+
+    return output;
+}
+
+let rect = rectangle(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
