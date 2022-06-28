fa13c495cc22a38628dee265cbd33ed4b9f72234
kovachevx <hristo.kovachev95@gmail.com>
2022-06-28 13:52:53 +0300

    Practising DOM manipulations.

---
+++ Programming-JS-Advanced/05. DOM-Events-Exercise/01. Sections/script.js
@@ -0,0 +1,16 @@
+function create(words) {
+   let content = document.getElementById('content');
+   for (let word of words) {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.textContent = word;
+      p.style.display = 'none';
+      div.appendChild(p);
+      div.addEventListener('click', onClick);
+      content.appendChild(div);
+   }
+
+   function onClick(ev) {
+      ev.target.children[0].style.display = '';
+   }
+}
