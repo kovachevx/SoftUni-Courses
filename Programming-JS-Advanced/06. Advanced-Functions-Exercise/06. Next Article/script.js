bcfaab4d0e08348ab8eb17c591627f8ba076e7e3
kovachevx <hristo.kovachev95@gmail.com>
2022-07-05 18:48:52 +0300

    Fetching the next article by clicking a button.

---
+++ Programming-JS-Advanced/06. Advanced-Functions-Exercise/06. Next Article/script.js
@@ -0,0 +1,10 @@
+function getArticleGenerator(articles) {
+    let content = document.getElementById('content');
+    return function showNext() {
+        if (articles.length !== 0) {
+            let article = document.createElement('article');
+            article.textContent = articles.shift();
+            content.appendChild(article);
+        }
+    }
+}
