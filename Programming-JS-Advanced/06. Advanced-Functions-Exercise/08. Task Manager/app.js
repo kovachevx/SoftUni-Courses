43efe465cbf65901ac2fff83e4ddd24b814f6fa9
kovachevx <hristo.kovachev95@gmail.com>
2022-07-05 18:49:42 +0300

    Modifying the DOM tree.

---
+++ Programming-JS-Advanced/06. Advanced-Functions-Exercise/08. Task Manager/app.js
@@ -0,0 +1,62 @@
+function solve() {
+
+    const input = {
+        name: document.getElementById('task'),
+        description: document.getElementById('description'),
+        date: document.getElementById('date'),
+    };
+
+    const [_, openSection, progressSection, finishSection] = Array.from(document.querySelectorAll('section')).map(e => e.children[1]);
+    document.getElementById('add').addEventListener('click', addTask);
+
+    function addTask(event) {
+        event.preventDefault();
+
+        const article = document.createElement('article');
+        article.appendChild(createElement('h3', input.name.value));
+        article.appendChild(createElement('p', `Description: ${input.description.value}`));
+        article.appendChild(createElement('p', `Due Date: ${input.date.value}`));
+        const div = createElement('div', '', 'flex');
+
+        const startButton = createElement('button', 'Start', 'green', onStart);
+        const deleteButton = createElement('button', 'Delete', 'red', onDelete);
+        const finishButton = createElement('button', 'Finish', 'orange', onFinish);
+
+        div.appendChild(startButton);
+        div.appendChild(deleteButton);
+        article.appendChild(div);
+        openSection.appendChild(article);
+
+        Object.values(input).forEach(i => i.value = '');
+
+        function onDelete() {
+            article.remove();
+        }
+
+        function onStart() {
+            startButton.remove();
+            div.appendChild(finishButton);
+            progressSection.appendChild(article);
+        }
+
+        function onFinish() {
+            div.remove();
+            finishSection.appendChild(article);
+        }
+    }
+
+    function createElement(type, content, className, eventListener) {
+        const element = document.createElement(type);
+        element.textContent = content;
+
+        if (className) {
+            element.className = className;
+        }
+
+        if (eventListener) {
+            element.addEventListener('click', eventListener)
+        }
+
+        return element;
+    }
+}
